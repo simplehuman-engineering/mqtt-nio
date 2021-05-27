3e095781afcffd364ac5327512e39d09062c275c
Tarek Sabry <tareksabry444@outlook.com>
2021-05-27 12:07:18 +0200

    Remove NIOSSL condition

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "NIOHTTP1", package: "swift-nio"),
             .product(name: "NIOWebSocket", package: "swift-nio"),
-            .product(name: "NIOSSL", package: "swift-nio-ssl", condition: .when(platforms: [.linux, .macOS])),
+            .product(name: "NIOSSL", package: "swift-nio-ssl"),
             .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
         ]),
         .target(name: "CCoreMQTT", dependencies: []),
